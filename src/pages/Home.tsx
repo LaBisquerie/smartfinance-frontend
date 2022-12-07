1b1f4d5152025ebbcfca6f7d7027dd793782e34e
Ali Chamass <ali.chamas@ynov.com>
2022-12-07 23:30:05 +0100

    Work in progress

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
-export interface IHomePageProps {}
+export interface IHomePageProps { }
 
 const HomePage: React.FunctionComponent<IHomePageProps> = () => {
     return (
-        <div>
-            <p>C'est la home page a ta mere la pute</p>
+        <div className='home-main-container'>
+            <div className="cards-container">
+                <div className="card">
+                    <div className="card-body">
+                        <blockquote className="blockquote mb-0">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer className="blockquote-footer mt-3">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="card-body">
+                        <blockquote className="blockquote mb-0">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer className="blockquote-footer mt-3">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="card-body">
+                        <blockquote className="blockquote mb-0">
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer className="blockquote-footer mt-3">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
